775c355c9100aee0f59f05852f07a1a4f0663345
Thomas Koch <thomas@koch.ro>
2010-12-08 15:41:19 +0100

    repair HtmlForm button element

--- HtmlForm/src/elements/button.php
+++ HtmlForm/src/elements/button.php
@@ -4,11 +4,16 @@ class ymcHtmlFormElementButton extends ymcHtmlFormElementBase
 {
     protected $type = 'submit';
 
+    private $form;
+
     public function init( ymcHtmlForm $form, ymcHtmlFormInputSource $inputSource = NULL )
     {
         $form->registerOnInit( $this );
-        if( $inputSource )
-        {
+        $this->form = $form;
+    }
+
+    public function validate( ymcHtmlFormInputSource $inputSource )
+    {
             $options = $this->options;
             $this->value = $inputSource->get( $this->name, 
                                               $options->filter,
@@ -16,9 +21,8 @@ class ymcHtmlFormElementButton extends ymcHtmlFormElementBase
 
             if( $this->value )
             {
-                $form->setButton( $this );
+                $this->form->setButton( $this );
             }
-        }
-        return array();
+            return array();
     }
 }
