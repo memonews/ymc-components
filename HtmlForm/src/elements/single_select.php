c36fd1f7c5de277e377dfb85079db38e1b814ed8
nils.kuebler <nils.kuebler@memonews.com>
2012-12-20 15:52:14 +0100

    allow to override emptyFailures in select-boxes.

--- HtmlForm/src/elements/single_select.php
+++ HtmlForm/src/elements/single_select.php
@@ -4,20 +4,23 @@ class ymcHtmlFormElementSingleSelect extends ymcHtmlFormElementBase
 {
     protected $type = 'select';
 
-    public function __construct( $name )
-    {
-        parent::__construct( $name );
-    }
-
     protected function filter( ymcHtmlFormInputSource $inputSource )
     {
         $options = $this->options;
-
+        $emptyFailure = $options->emptyFailure;
+        
         $value = $inputSource->get( $this->name, 
                                     $options->filter,
                                     $options->filterOptions );
         
-        if( !in_array( $value, $this->values ) )
+        if( empty( $value ) )
+        {
+        	if( $emptyFailure )
+        	{
+        		$this->failures[] = new $emptyFailure( $this );
+        	}
+        }
+        else if( !in_array( $value, $this->values ) )
         {
             $value = '';
             $this->failures[] = new ymcHtmlFormFailureNotInSet( $this );
